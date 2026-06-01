cb7c09f31cd592685a151f9aa201edfc188ae6dd
vaibhav gaur <gaurvaibhav357@gmail.com>
2026-06-01 17:54:32 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+    int year;
+
+    printf("enter year = ");
+    scanf("%d",&year);
+
+    if(year%4==0) {
+        if(year%100==0) {
+            if(year%400==0) {
+                printf("leap year");
+            } else {
+                printf("not leap year");
+            }
+        } else {
+            printf("leap year");
+        }
+    } else {
+        printf("not leap year");
+    }
+
+
+
+    return 0;
+}
