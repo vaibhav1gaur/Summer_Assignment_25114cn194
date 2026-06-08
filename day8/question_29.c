917481e12a2a207be3ef1ebb580e71d4cd3d053b
vaibhav gaur <gaurvaibhav357@gmail.com>
2026-06-08 19:46:55 +0530

    Add files via upload

---
+++ day8/question_29.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int num;
+    printf("enter a number = ");
+    scanf("%d",&num);
+
+    for(int i=0; i<num; i++) {
+        for(int j=0; j<=i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
