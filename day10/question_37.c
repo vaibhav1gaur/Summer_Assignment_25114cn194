84f505371ac4247c8668c89f05386d7bde2cf4ac
vaibhav gaur <gaurvaibhav357@gmail.com>
2026-06-10 21:46:02 +0530

    Add files via upload

---
+++ day10/question_37.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n;
+    printf("Enter the size: ");
+    scanf("%d", &n);
+    
+    for(int i=0; i<n; i++) {
+        for(int j=n-i-1; j>0; j--) {
+            printf(" ");
+        }
+        for(int j=0; j<i+1; j++) {
+            printf("*");
+        }
+        for(int j=1; j<i+1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
