058794888480e0bb9c4e0c05fa967a6362bdbdad
vaibhav gaur <gaurvaibhav357@gmail.com>
2026-06-11 21:05:19 +0530

    Add files via upload

---
+++ day11/ques_41.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int find_sum(int num1, int num2) {
+    return num1 + num2;
+}
+
+int main() {
+    
+    printf("the sum of 6 and 48 is: %d",find_sum(6,48));
+    
+    return 0;
+}
