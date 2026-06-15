4115b96f4d3995965d599cf1e38c13f098074b4e
vaibhav gaur <gaurvaibhav357@gmail.com>
2026-06-15 22:07:48 +0530

    Add files via upload

---
+++ day15/ques_57.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void reverseArray(int arr[], int size) {
+    for (int i = 0; i < size / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[size - 1 - i];
+        arr[size - 1 - i] = temp;
+    }
+}
+
+int main() {
+
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    reverseArray(arr, size);
+
+    printf("Reversed array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
