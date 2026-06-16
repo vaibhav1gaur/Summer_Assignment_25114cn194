f4c6d18b741bbabb1f0f1cd1b472e915550eea05
vaibhav gaur <gaurvaibhav357@gmail.com>
2026-06-16 22:05:06 +0530

    Add files via upload

---
+++ day16/ques_64.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void remove_duplicates(int arr[], int n) {
+    int temp[n];
+    int j = 0;
+
+    for(int i=0; i<n-1; i++) {
+        if(arr[i] != arr[i+1]) {
+            temp[j++] = arr[i];
+        }
+    }
+    temp[j++] = arr[n-1];
+
+    for(int i=0; i<j; i++) {
+        printf("%d ", temp[i]);
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 2, 3, 4, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    remove_duplicates(arr, n);
+
+    return 0;
+}
