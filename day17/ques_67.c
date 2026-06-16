c94b440ce44bb2c0e8f30bcb95db13498d35b734
vaibhav gaur <gaurvaibhav357@gmail.com>
2026-06-16 22:07:05 +0530

    Add files via upload

---
+++ day17/ques_67.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void intersection_arr(int arr1[], int arr2[], int n1, int n2, int arr3[], int *n3) {
+    int i = 0, j = 0, k = 0;
+
+    while (i < n1 && j < n2) {
+        if (arr1[i] < arr2[j]) {
+            i++;
+        } else if (arr1[i] > arr2[j]) {
+            j++;
+        } else {
+            arr3[k++] = arr1[i++];
+            j++;
+        }
+    }
+
+    *n3 = k; // Update the size of the intersection array
+}
+
+int main() {
+    int arr1[] = {1, 2, 4, 5, 6};
+    int arr2[] = {2, 3, 5, 7};
+    int n1 = sizeof(arr1) / sizeof(arr1[0]);
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+    int arr3[n1 < n2 ? n1 : n2]; // Maximum possible size of intersection
+    int n3; // Size of the intersection array
+
+    intersection_arr(arr1, arr2, n1, n2, arr3, &n3);
+
+    printf("Intersection of the two arrays: ");
+    for (int i = 0; i < n3; i++) {
+        printf("%d ", arr3[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
