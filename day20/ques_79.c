c17b956afdce01e056bd945e93488ad7d7822e98
vaibhav gaur <gaurvaibhav357@gmail.com>
2026-06-20 22:46:12 +0530

    Add files via upload

---
+++ day20/ques_79.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int row=3,col=3;
+
+    for(int i=0; i<row; i++) {
+        int r_sum=0;
+        for(int j=0; j<col; j++) {
+            r_sum += arr[i][j];
+        }
+        printf("%d\n",r_sum);
+    }
+
+    
+
+
+    return 0;
+}
